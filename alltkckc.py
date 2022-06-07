61701d84041cdba270d48144f0fbd8f09c74ae73
yogeshbendre <07.yogesh@gmail.com>
2022-06-07 16:21:19 +0530

    Update alltkckc.py

--- alltkckc.py
+++ alltkckc.py
@@ -1,1 +1,19 @@
 #Python
+
+import subprocess
+
+alltkc = ""
+with open("list.txt", "r") as fp:
+  alltkc=fp.read()
+
+alltkc=alltkc.split("\n")
+
+for s in alltkc[1:]:
+  a = s.split(' ')
+  ns = a[0]
+  tkc = a[-1]
+  print(tkc)
+  cmd = "kubectl get secret -n "+ns+" "+tkc+"-kubeconfig -o jsonpath='{.data.value}' | base64 -d > "+tkc+"-kubeconfig"
+  proc = subprocess.Popen(cmd,shell=True,stdin=subprocess.PIPE,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+  my_status, stderr_value = proc.communicate()
+  my_status = my_status.decode("utf-8").strip()
