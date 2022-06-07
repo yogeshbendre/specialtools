b027a2d88c96f29869ed2e967088149dab3b637e
yogeshbendre <07.yogesh@gmail.com>
2022-06-07 16:37:38 +0530

    Update alltkclist.sh

--- alltkclist.sh
+++ alltkclist.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mkdir alltkc
+mkdir alltkc || true
 cd alltkc
 kubectl get tkc -A -o=custom-columns="NS:.metadata.namespace,NAME:.metadata.name" > list.txt
 wget https://raw.githubusercontent.com/yogeshbendre/specialtools/master/alltkckc.py -O alltkckc.py
