69fc6758f733bb484f099a542be6eb662e1266ae
yogeshbendre <07.yogesh@gmail.com>
2022-06-08 15:44:01 +0530

    Update alltkckc.py

--- alltkckc.py
+++ alltkckc.py
@@ -19,7 +19,7 @@ for s in alltkc[1:]:
   my_status = my_status.decode("utf-8").strip()
   
   #kubectl get secret -n $ns $c-ssh -o jsonpath='{.data.ssh-privatekey}' | base64 -d > $c-ssh
-  cmd = "kubectl get secret -n "+ns+" "+tkc+"-ssh -o jsonpath='{.data.ssh-privatekey}' | base64 -d > "+tkc+"-ssh"
+  cmd = "kubectl get secret -n "+ns+" "+tkc+"-ssh -o jsonpath='{.data.ssh-privatekey}' | base64 -d > "+tkc+"-ssh" + " ; chmod 600 " + tkc + "-ssh"
   proc = subprocess.Popen(cmd,shell=True,stdin=subprocess.PIPE,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
   my_status, stderr_value = proc.communicate()
   my_status = my_status.decode("utf-8").strip()
