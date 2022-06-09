1695ac52f2d1e43d1c3584796e70603ff4874d79
yogeshbendre <07.yogesh@gmail.com>
2022-06-09 11:44:01 +0530

    Update scoremerger.py

--- scoremerger.py
+++ scoremerger.py
@@ -1,1 +1,17 @@
+import argparse
+
+
+
+
+
+def main():
+    parser=argparse.ArgumentParser(description='Score Merger')
+    parser.add_argument("-fw", "--folder", help="Folder path", required=True)
+    parser.add_argument("-v", "--vc", help= "VC Name", required=True)
+    parser.add_argument("-w", "--wcp", help= "WCP Cluster", required=True)
+
+    args2 = parser.parse_args()
+    args = vars(args2)
+    wcp_cluster_name = args['wcpClusterName']
+    wcp_namespace_prefix = args['wcpNsPrefix']
 
