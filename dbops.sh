53a0dd7292a3ea0492157772539788db103fe0fc
yogeshbendre <07.yogesh@gmail.com>
2023-08-28 22:10:18 +0530

    Create dbops.sh

---
+++ dbops.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "Creating tasks"
+curl -X POST -d "{\"task\": \"Task 1\"}" tasksapp-svc.workload-mongodbwebapptransactions.svc.cluster.local:8080/task
+curl -X POST -d "{\"task\": \"Task 2\"}" tasksapp-svc.workload-mongodbwebapptransactions.svc.cluster.local:8080/task
+curl -X POST -d "{\"task\": \"Task 3\"}" tasksapp-svc.workload-mongodbwebapptransactions.svc.cluster.local:8080/task
+curl -X POST -d "{\"task\": \"Task 4\"}" tasksapp-svc.workload-mongodbwebapptransactions.svc.cluster.local:8080/task
+curl -X POST -d "{\"task\": \"Task 5\"}" tasksapp-svc.workload-mongodbwebapptransactions.svc.cluster.local:8080/task
+curl tasksapp-svc.workload-mongodbwebapptransactions.svc.cluster.local:8080/tasks
+echo "sleeping for 10s"
+sleep 10s
+echo "Deleting tasks"
+curl -X POST tasksapp-svc.workload-mongodbwebapptransactions.svc.cluster.local:8080/tasks/delete
+echo "Done DB Ops"
